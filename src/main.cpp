477502a8b1069c8789c9f0a2c0d607bb7a764239
mc08 <118325498+splitlane@users.noreply.github.com>
2025-02-01 21:29:33 -0800

    motor class!

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
+#include "motor.cpp"
 #include <Arduino.h>
 
-int motor1Pin1 = 27; 
-int motor1Pin2 = 26; 
-int enable1Pin = 14; 
+// TODO: config pins
+Motor frontLeftMotor(-1, -1, -1);
+Motor frontRightMotor(-1, -1, -1);
+Motor backLeftMotor(-1, -1, -1);
+Motor backRightMotor(-1, -1, -1);
 
-const int freq = 30000;
-const int pwmChannel = 0;
-const int resolution = 8;
-int dutyCycle = 200;
+// TODO: get pwm for servo working
+// const int freq = 30000;
+// const int pwmChannel = 0;
+// const int resolution = 8;
+// int dutyCycle = 200;
 
 void setup() {
-  pinMode(motor1Pin1, OUTPUT);
-  pinMode(motor1Pin2, OUTPUT);
-  pinMode(enable1Pin, OUTPUT);
-
   Serial.begin(115200);
 
   Serial.print("Testing DC Motor...");
@@ -22,37 +22,35 @@ void setup() {
 void loop() {
   // Move the DC motor forward at maximum speed
   Serial.println("Moving Forward");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH); 
+  frontLeftMotor.setPower(255);
+  frontRightMotor.setPower(255);
+  backLeftMotor.setPower(255);
+  backRightMotor.setPower(255);
   delay(2000);
 
   // Stop the DC motor
   Serial.println("Motor stopped");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
+  frontLeftMotor.setPower(0);
+  frontRightMotor.setPower(0);
+  backLeftMotor.setPower(0);
+  backRightMotor.setPower(0);
   delay(1000);
 
   // Move DC motor backwards at maximum speed
   Serial.println("Moving Backwards");
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW); 
+  frontLeftMotor.setPower(-255);
+  frontRightMotor.setPower(-255);
+  backLeftMotor.setPower(-255);
+  backRightMotor.setPower(-255);
   delay(2000);
 
   // Stop the DC motor
   Serial.println("Motor stopped");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
+  frontLeftMotor.setPower(0);
+  frontRightMotor.setPower(0);
+  backLeftMotor.setPower(0);
+  backRightMotor.setPower(0);
   delay(1000);
 
-  // Move DC motor forward with increasing speed
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  while (dutyCycle <= 255){
-    ledcWrite(enable1Pin, dutyCycle);   
-    Serial.print("Forward with duty cycle: ");
-    Serial.println(dutyCycle);
-    dutyCycle = dutyCycle + 5;
-    delay(500);
-  }
-  dutyCycle = 200;
+
 }
---
+++ src/motor.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+
+class Motor {
+    public:
+        Motor(uint8_t pinIN1, uint8_t pinIN2, uint8_t pinEN) : pinIN1(pinIN1), pinIN2(pinIN2), pinEN(pinEN) {
+            pinMode(pinIN1, OUTPUT);
+            pinMode(pinIN2, OUTPUT);
+            pinMode(pinEN, OUTPUT);
+
+            digitalWrite(pinEN, LOW);
+        };
+        // power is from -255 to 255
+        // used int instead of int8_t due to range
+        void setPower(int power) {
+            if (power >= 0) {
+                digitalWrite(pinIN1, HIGH);
+                digitalWrite(pinIN2, LOW);
+
+                analogWrite(pinEN, power);
+            } else {
+                digitalWrite(pinIN1, LOW);
+                digitalWrite(pinIN2, HIGH);
+
+                analogWrite(pinEN, -power);
+            }
+        };
+        void brake() {
+            digitalWrite(pinIN1, LOW);
+            digitalWrite(pinIN2, LOW);
+            digitalWrite(pinEN, HIGH);
+        };
+        void freeRun() {
+            digitalWrite(pinEN, LOW);
+        };
+    private:
+        uint8_t pinIN1, pinIN2, pinEN;
+};
